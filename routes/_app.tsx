e543c7050e08d5d8bc0489ea18d35594badaea51
Squix <6475960+Squix@users.noreply.github.com>
2025-09-06 18:27:46 +0200

    fix: remove default routes

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,7 +5,7 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>playnite_sync_explorer</title>
+        <title>Playnite sync explorer</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
