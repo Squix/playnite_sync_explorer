e28975c5a9a1cd5e1449238226df9f7fbf2a2039
Squix <6475960+Squix@users.noreply.github.com>
2025-09-06 16:11:44 +0200

    fix: tweak layout for a dark theme

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,7 +1,5 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
-import { getGamesFromDB } from "../utils/db.ts";
-
-type Game = { id: number; name: string; contents: Record<string, any> };
+import { Game, getGamesFromDB } from "../utils/db.ts";
 
 interface Data {
   games: Game[];
@@ -35,45 +33,68 @@ export const handler: Handlers<Data> = {
 export default function Home({ data }: PageProps<Data>) {
   const { games, query } = data;
   return (
-    <div class="p-8 max-w-3xl mx-auto">
-      <h1 class="text-2xl font-bold mb-4">Games</h1>
-      <form method="get" action="/" class="mb-4 flex gap-2 items-center">
-        <input
-          name="q"
-          value={query}
-          placeholder="Search..."
-          class="border px-2 py-1"
-        />
-        <button type="submit" class="px-3 py-1 border rounded">
-          Search
-        </button>
-        {query && (
-          <a
-            href="/"
-            class="px-3 py-1 border rounded bg-gray-100 text-gray-800 hover:bg-gray-200 transition"
-          >
-            Clear
-          </a>
-        )}
-      </form>
-      <table class="min-w-full bg-white shadow rounded">
-        <thead>
-          <tr class="text-left">
-            <th class="border px-4 py-2 w-12">Name</th>
-            <th class="border px-4 py-2 w-64">Genres</th>
-          </tr>
-        </thead>
-        <tbody>
-          {games.map((g) => (
-            <tr key={g.id} class="even:bg-gray-50">
-              <td class="border px-4 py-2 w-64">{g.name}</td>
-              <td class="border px-4 py-2 w-64">
-                {g.contents.Genres?.map((g: any) => g.Name).join(" | ")}
-              </td>
+    <div class="bg-gray-900 text-gray-100 min-h-screen w-full">
+      <div class="p-8 max-w-3xl mx-auto">
+        <h1 class="text-2xl font-bold mb-4">Games</h1>
+        <form method="get" action="/" class="mb-4 flex gap-2 items-center">
+          <input
+            name="q"
+            value={query}
+            placeholder="Search..."
+            class="border px-2 py-1"
+          />
+          <button type="submit" class="px-3 py-1 border rounded">
+            Search
+          </button>
+          {query && (
+            <a
+              href="/"
+              class="px-3 py-1 border rounded bg-gray-100 text-gray-800 hover:bg-gray-200 transition"
+            >
+              Clear
+            </a>
+          )}
+        </form>
+        <table class="min-w-full bg-gray-800 shadow rounded text-gray-100">
+          <thead>
+            <tr class="text-left">
+              <th class="border border-gray-700 px-4 py-2 w-12">Name</th>
+              <th class="border border-gray-700 px-4 py-2 w-64">Genres</th>
+              <th class="border border-gray-700 px-4 py-2 w-32">Stores</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {games.length === 0
+              ? (
+                <tr>
+                  <td
+                    class="border border-gray-700 px-4 py-4 text-center text-gray-400"
+                    colSpan={3}
+                  >
+                    no games found :(
+                  </td>
+                </tr>
+              )
+              : (
+                games.map((g) => (
+                  <tr key={g.id} class="even:bg-gray-700">
+                    <td class="border border-gray-700 px-4 py-2 w-64">
+                      {g.name}
+                    </td>
+                    <td class="border border-gray-700 px-4 py-2 w-64">
+                      {g.contents.Genres?.map((genre: any) => genre.Name).join(
+                        " | ",
+                      )}
+                    </td>
+                    <td class="border border-gray-700 px-4 py-2 w-32">
+                      {g.contents.Source?.Name}
+                    </td>
+                  </tr>
+                ))
+              )}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
--- utils/db.ts
+++ utils/db.ts
@@ -2,6 +2,32 @@ import { Client } from "@db/postgres";
 
 const DB_CONNECTION_STRING = Deno.env.get("DATABASE_URL") ?? "";
 
+export type GameContent = {
+  Id: string;
+  Name: string;
+  Tags: { Id: string; Name: string }[];
+  Added: string;
+  Links: { Url: string; Name: string }[];
+  GameId: string;
+  Genres: { Id: string; Name: string }[];
+  Source: { Id: string; Name: string };
+  Features: { Id: string; Name: string }[];
+  Modified: string;
+  PluginId: string;
+  Platforms: { Id: string; Name: string; SpecificationId: string }[];
+  AgeRatings: { Id: string; Name: string }[];
+  Developers: { Id: string; Name: string }[];
+  Publishers: { Id: string; Name: string }[];
+  Description: string;
+  ReleaseDate: string;
+  HasCoverImage: boolean;
+  CommunityScore: number;
+  CompletionStatus: { Id: string; Name: string };
+  HasBackgroundImage: boolean;
+};
+
+export type Game = { id: number; name: string; contents: GameContent };
+
 export async function getGamesFromDB() {
   const client = new Client(DB_CONNECTION_STRING);
   try {
@@ -10,11 +36,7 @@ export async function getGamesFromDB() {
     throw new Error("Database connection failed: " + (err?.message || err));
   }
   try {
-    const result = await client.queryObject<{
-      id: number;
-      name: string;
-      contents: Record<string, string>;
-    }>("SELECT id, name, contents FROM playnite_game ORDER BY name");
+    const result = await client.queryObject<Game>("SELECT id, name, contents FROM playnite_game ORDER BY name");
     return result.rows;
   } finally {
     await client.end();
